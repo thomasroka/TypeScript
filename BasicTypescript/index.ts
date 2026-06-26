f965439d7fcd2feb56386e10f58508af91d97faf
Thomas Roka <thomasroka360@gmail.com>
2026-06-26 18:08:26 +0545

    Learning string, numbers, boolean

--- BasicTypescript/index.ts
+++ BasicTypescript/index.ts
@@ -6,3 +6,18 @@ const person: Person = {
     name: "John",
 };
 console.log(person.name);
+
+// Lets start first with strings, Numbers ,boolean
+
+
+// For strings
+let myName: string = "Thomas Roka";
+console.log(myName)
+
+// for numbers 
+let numberss: number = 9;
+console.log(numberss)
+
+//Boolean
+let isHard: Boolean = false;
+console.log(isHard);
