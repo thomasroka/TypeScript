7c3a6f21ff899a87ab59d3ce68b4eaad2733175a
Thomas Roka <thomasroka360@gmail.com>
2026-07-01 17:01:13 +0545

    Learning tsconcepts

---
+++ TsConcepts/Typealias.ts
@@ -0,0 +1,11 @@
+//Type aliases let you name a type and reuse it. The syntax is: type AliasName = <your type here>
+/*
+ with type alias — clean and reusable
+type Student = { name: string; age: number };
+
+let user1: Student = { name: "Tom", age: 20 };
+let user2: Student = { name: "Sara", age: 22 };
+
+function getUser(): Student { ... }
+function saveUser(user: Student) { ... }
+ */
--- TsConcepts/Unions.ts
+++ TsConcepts/Unions.ts
@@ -8,7 +8,6 @@ console.log(info);
 informations(100);
 
 // Type narrowing in ts
-
 //let info: string|number='Thomas' here ts  accepted both number and string but to work with it it need to know its type so we use type of  
 
 function operations(fullname:string|number,num:string|number){
